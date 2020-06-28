d60175f5da261ee8d19a799f20c40ff87b31c110
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-06-28 15:44:35 +0200

    Allow to override SSID* from commandline

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -71,10 +71,14 @@
 #define WIFI_DNS               "192.168.1.1"     // [IpAddress4] If not using DHCP set DNS1 IP address (might be equal to WIFI_GATEWAY)
 #define WIFI_DNS2              "0.0.0.0"         // [IpAddress5] If not using DHCP set DNS2 IP address (might be equal to WIFI_GATEWAY)
 
-#define STA_SSID1              ""                // [Ssid1] Wi-Fi SSID
-#define STA_PASS1              ""                // [Password1] Wi-Fi password
-#define STA_SSID2              ""                // [Ssid2] Optional alternate AP Wi-Fi SSID
-#define STA_PASS2              ""                // [Password2] Optional alternate AP Wi-Fi password
+#define STA_SSID1              ""                // [Ssid1] Wifi SSID
+#ifndef STA_PASS1
+#define STA_PASS1              ""                // [Password1] Wifi password
+#endif
+#define STA_SSID2              ""                // [Ssid2] Optional alternate AP Wifi SSID
+#ifndef STA_PASS2
+#define STA_PASS2              ""                // [Password2] Optional alternate AP Wifi password
+#endif
 #define WIFI_AP_PASSPHRASE     ""                // AccessPoint passphrase. For WPA2 min 8 char, for open use "" (max 63 char).
 #define WIFI_CONFIG_TOOL       WIFI_RETRY        // [WifiConfig] Default tool if Wi-Fi fails to connect (default option: 4 - WIFI_RETRY)
                                                  // (WIFI_RESTART, WIFI_MANAGER, WIFI_RETRY, WIFI_WAIT, WIFI_SERIAL, WIFI_MANAGER_RESET_ONLY)
