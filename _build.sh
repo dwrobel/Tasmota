a9938db43a5d56dc1623315eda5e1ff66f5743f2
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-06-28 15:44:59 +0200

    Allow to pass passwords from commandline

--- _build.sh
+++ _build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export PLATFORMIO_BUILD_FLAGS='-DUSE_CONFIG_OVERRIDE -DSTA_PASS1=\"\" -DSTA_PASS2=\"\"'
+
 for t in tasmota-minimal tasmota-sensors; do
     pio run -e ${t}
     cp -pf .pioenvs/${t}/firmware.bin ${t}.bin
