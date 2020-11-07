8fdf8519bacb9d141867bccbbfdbdfe73644ea57
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-11-07 10:04:13 +0100

    build only tasmota-sensors

--- _build.sh
+++ _build.sh
@@ -3,8 +3,15 @@
 set -e
 
 export PLATFORMIO_BUILD_FLAGS='-DUSE_CONFIG_OVERRIDE -DSTA_PASS1=\"\" -DSTA_PASS2=\"\"'
+#pio lib install mbed-sam-grove/LinkedList
 
-for t in tasmota-minimal tasmota-sensors; do
+#for t in tasmota-minimal tasmota-sensors tasmota32; do
+for t in tasmota32; do
     pio run -e ${t}
-    cp -pf .pioenvs/${t}/firmware.bin ${t}.bin
+    cp -pf .pio/build/${t}/firmware.bin ${t}.bin
+    gzip -f ${t}.bin
+
+    if [ -d ~/public_html/tasmota/ ]; then
+        cp -a ${t}.bin.gz ~/public_html/tasmota/
+    fi
 done
