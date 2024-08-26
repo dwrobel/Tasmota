60640ee0e8407464dcd106f44ade34755ba417b4
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2024-08-26 11:40:43 +0200

    Add branch info to footer

--- tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
@@ -408,7 +408,7 @@ const char HTTP_COUNTER[] PROGMEM =
   "<br><div id='t' style='text-align:center;'></div>";
 
 const char HTTP_END[] PROGMEM =
-  "<div style='text-align:right;font-size:11px;'><hr/><a href='https://bit.ly/tasmota' target='_blank' style='color:#aaa;'>Tasmota %s %s " D_BY " Theo Arends</a></div>"
+  "<div style='text-align:right;font-size:11px;'><hr/><a href='https://bit.ly/tasmota' target='_blank' style='color:#aaa;'>Tasmota %s (branch: dw-14.2.0.3-development-ds18x20-ext-20240823) %s " D_BY " Theo Arends</a></div>"
   "</div>"
   "</body>"
   "</html>";
