1eb81108eb5d953e56e111b958fff93e0eee7230
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2025-05-17 10:30:51 +0200

    Add building tasmota32-webcam & bump version dir

--- _build.sh
+++ _build.sh
@@ -8,7 +8,7 @@ export PLATFORMIO_BUILD_FLAGS='-DUSE_CONFIG_OVERRIDE -DSTA_PASS1=\"\" -DSTA_PASS
 # For ESP32-D0WDQ6 v1.0 (PSRAM disabled) (https://github.com/LilyGO/TTGO-T8-ESP32.git)
 # use "board = esp32-fix" in platformio_tasmota_env32.ini for [env:tasmota32].
 
-for t in tasmota-minimal tasmota-sensors tasmota32 tasmota32solo1 tasmota32s2 tasmota32s3; do
+for t in tasmota-minimal tasmota-sensors tasmota32 tasmota32solo1 tasmota32s2 tasmota32s3 tasmota32-webcam; do
     pio run -e ${t}
     cp -pf .pio/build/${t}/firmware.bin ${t}.bin
     gzip -c ${t}.bin >${t}.bin.gz
@@ -18,7 +18,7 @@ for t in tasmota-minimal tasmota-sensors tasmota32 tasmota32solo1 tasmota32s2 ta
     fi
 
     if [ -d ~/public_html/tasmota/ ]; then
-        REL_DIR=~/public_html/tasmota/14.2.0.3-5
+        REL_DIR=~/public_html/tasmota/14.6.0.1-1
         mkdir -p ${REL_DIR}
         cp -a ${t}.bin ${t}.bin.gz ${REL_DIR}/
 
