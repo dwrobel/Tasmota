98543674da5b8a1af4e931581cac601aaf512813
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2025-05-23 16:05:19 +0200

    Bump release

--- tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
@@ -439,7 +439,7 @@ const char HTTP_COUNTER[] PROGMEM =
   "<br><div id='t' style='text-align:center;'></div>";
 
 const char HTTP_END[] PROGMEM =
-  "<div style='text-align:right;font-size:11px;'><hr/><a href='https://bit.ly/tasmota' target='_blank' style='color:#aaa;'>Tasmota %s (branch: dw-14.6.0.2-development-ds18x20-ext-20250517 rel: 1) %s " D_BY " Theo Arends</a></div>"
+  "<div style='text-align:right;font-size:11px;'><hr/><a href='https://bit.ly/tasmota' target='_blank' style='color:#aaa;'>Tasmota %s (branch: dw-14.6.0.2-development-ds18x20-ext-20250517 rel: 2) %s " D_BY " Theo Arends</a></div>"
   "</div>"
   "</body>"
   "</html>";
